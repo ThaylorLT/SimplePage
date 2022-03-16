4b4b1f53f922fb24a2b16bf5969cad1ed162694d
ThaylorLT <91397478+ThaylorLT@users.noreply.github.com>
2022-03-15 21:30:07 -0300

    Update time.js

--- time.js
+++ time.js
@@ -3,11 +3,17 @@ function clock(){
     var m = new Date().getMinutes();
     var d = new Date().getDate();
     var mo = new Date().getMonth();
+    
 
     var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     document.getElementById("hour").innerHTML = h + ' : ' + m;
     document.getElementById("month").innerHTML = monthNames[mo] + ' ' + d;
+    if (h<12){
+     document.getElementById("name").innerHTML= 'Good Morning Thaylor!'
+    } else if (h>12){
+        document.getElementById("hour").innerHTML = 'Good Night Thaylor!'
+    }
 }
 
 var interval = setInterval(clock, 1000)
